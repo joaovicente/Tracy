75e69950ddb7cf53f265f0931050fd6fe3954146
Vicente <joao.diogo.vicente@gmail.com>
2014-06-15 00:58:53 +0100

    Defined next tasks as comments

--- src/main/java/com/apm4all/tracy/TracyThreadContext.java
+++ src/main/java/com/apm4all/tracy/TracyThreadContext.java
@@ -3,6 +3,12 @@ package com.apm4all.tracy;
 public class TracyThreadContext {
 	private String taskId;
 	private String parentOptId;
+	//TODO: Stack of TracyEvent(s)
+	//TODO: List of popped TracyEvent(s)
+	//TODO: Capture hostname
+	//TODO: Operation name to be captured as label of Tracy.Before() and TracyAfter
+	//TODO: Component name would be useful to gather (1 for the whole context)
+	//TODO: Consider mechanism to relay TaskId and parentOptId to child worker threads as well as getting worker thread events back to main thread 
 	
 	public TracyThreadContext(String taskId, String parentOptId) {
 		super();
