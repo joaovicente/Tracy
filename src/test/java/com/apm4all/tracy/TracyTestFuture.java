030e9719ca3d6e03271458c4e5b154016f502fdb
Vicente <joao.diogo.vicente@gmail.com>
2014-06-22 23:50:59 +0100

    Futures test gets Long futures

--- src/test/java/com/apm4all/tracy/TracyTestFuture.java
+++ src/test/java/com/apm4all/tracy/TracyTestFuture.java
@@ -2,6 +2,7 @@ package com.apm4all.tracy;
 
 import static org.junit.Assert.*;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.Callable;
 import java.util.concurrent.CompletionService;
@@ -18,43 +19,46 @@ public class TracyTestFuture {
 	static final String PARENT_OPT_ID = "AAAA";
 	static final String L1_LABEL_NAME = "L1 Operation";
 	static final String L11_LABEL_NAME = "L11 Operation";
-	private ExecutorService executorService = Executors.newFixedThreadPool(NTHREDS);
+	private	ExecutorService executor = Executors.newFixedThreadPool(NTHREDS);
 
 	//TODO: Create ctx = Tracy.createFutureTheadContext();
 	//TODO: Create TracyableFuture interface providing getData() and getTracyThreadContext()
 	//TODO: Create Tracy.mergeFutureThreadContext(ctx);
 	
 	private Future<Long> retrieve()	{
-		Callable<Long> task = null;
-		task = new Callable<Long>() {
-			
+		Callable<Long> worker = null;
+		System.out.println("Executing future");
+		worker = new Callable<Long>() {
 			public Long call() throws Exception {
+				System.out.println("Executing future (call)");
 				Long someInteger = System.currentTimeMillis();
 				return someInteger;
 			}
 		};
-		return executorService.submit(task);
+		return executor.submit(worker);
 	}
 	
 	@Test
 	public void testFutureTrace() {
 		//FIXME: Future test blocking
-//		final int NUM_FUTURES = 2;
-//		int i;
-//		try {
-//			CompletionService<Long> completionService = 
-//			    	new ExecutorCompletionService<Long>(executorService);
-//			
-//			for (i=0; i<NUM_FUTURES ; i++)	{
-//				Future<Long> longFuture = retrieve();
-//			}
-//			
-//			for (i=0; i<NUM_FUTURES ; i++)	{
-//				Long Long = completionService.take().get();
-//			}
-//		} catch (Exception e) {
-//			throw new RuntimeException(e);
-//		}
+		final int NUM_FUTURES = 2;
+		ArrayList<Future<Long>> futuresList = new ArrayList<Future<Long>>();
+		int i;
+		try {
+			for (i=0; i<NUM_FUTURES ; i++)	{
+				System.out.println("Calling future");
+				futuresList.add(retrieve());
+			}
+			Thread.sleep(1000);
+			
+			for (Future<Long> future : futuresList)	{
+				System.out.println("Polling future");
+				Long l =  future.get();
+				System.out.println("Got " + l);
+			}
+		} catch (Exception e) {
+			throw new RuntimeException(e);
+		}
 	}
 	
 }
